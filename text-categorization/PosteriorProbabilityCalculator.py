dad54cd70e53203f9f955acbd2b29103c518ab11
Girish <girishc132004@gmail.com>
2014-10-16 21:44:27 -0400

    corrected calculations for each doc

--- text-categorization/PosteriorProbabilityCalculator.py
+++ text-categorization/PosteriorProbabilityCalculator.py
@@ -18,53 +18,56 @@ class PosteriorProbCalc():
          # vocabCalc.calculate(dir)
          #
          # print "Test class vocab count: ", vocabCalc.getClassVocabCount()
-
+         testDir = "earn"
          for classDir in os.listdir(dir):
-             classVocab = {}
-             currentCmap = {}
-             # if classDir == 'earn':
+             # if classDir == testDir:
+             if 1:
+                 numberOfFiles = len(os.listdir(os.path.join(dir, classDir)))
+                 correctClassEstimation = 0
+                 for filename in os.listdir(os.path.join(dir, classDir)):
+                     docVocab = {}
+                     currentCmap = {}
+                     testFile = open(os.path.join(dir, classDir, filename), "r")
 
-             filename = classDir + "_mega.txt"
-             testFile = open(os.path.join(dir, classDir, filename), "r")
+                     for line in testFile:
+                         words = line.split()
 
-             for line in testFile:
-                 words = line.split()
-
-                 for word in words:
-                     if word in classVocab:
-                         classVocab[word] += 1
-                     else:
-                         classVocab[word] = 1
-
-
-             # calculate currentCmap
-             totalCmap[classDir] = {}
-             tempFile = open("temp.txt", "w")
-             for trainClass, classPrior in priors.iteritems():
-                 prod = np.float128(1.0)
-
-                 for word, count in classVocab.iteritems():
-                     # print word, count
-                     if word in condProb[trainClass]:
-                         prod = prod * condProb[trainClass][word]
-                         # print prod
-                         # if condProb[trainClass][word] == 0.0:
-                         # print trainClass, word, condProb[trainClass][word]
-                         tempFile.write(trainClass + " " + word + " " + str(condProb[trainClass][word]) + " "
-                                        + str(prod))
-                         tempFile.write("\n")
+                         for word in words:
+                             if word in docVocab:
+                                 docVocab[word] += 1
+                             else:
+                                 docVocab[word] = 1
 
 
-                 currentCmap[trainClass] = prod * np.float128(classPrior)
+                     # calculate currentCmap
+                     totalCmap[filename] = {}
+                     tempFile = open("temp.txt", "w")
+                     for trainClass, classPrior in priors.iteritems():
+                         prod = np.float128(1.0)
 
-             # print "Actual Test class: ", classDir, ", Estimated Test class: " , max(currentCmap.iteritems(), key=operator.itemgetter(1))[0]
-                 totalCmap[classDir] = currentCmap
-             # print "Current cmap: ", currentCmap
-             # print "max: ", max(currentCmap.values())
-             # print "\n"
+                         for word, count in docVocab.iteritems():
+                             # print word, count
+                             if word in condProb[trainClass]:
+                                 prod = prod * condProb[trainClass][word]
+                                 # print prod
+                                 # if condProb[trainClass][word] == 0.0:
+                                 # print trainClass, word, condProb[trainClass][word]
+                                 tempFile.write(trainClass + " " + word + " " + str(condProb[trainClass][word]) + " "
+                                                + str(prod))
+                                 tempFile.write("\n")
 
-             print "Actual Test class: ", classDir, ", Estimated Test class: " , max(currentCmap.iteritems(),
-             key=operator.itemgetter(1))[0]
-             # print "\n"
+
+                         currentCmap[trainClass] = prod * np.float128(classPrior)
+
+                     totalCmap[filename] = currentCmap
+                     estimatedClass = max(currentCmap.iteritems(), key=operator.itemgetter(1))[0]
+                     # print "Actual file class: ", classDir, ", Estimated file class: " , estimatedClass
+                     # print "\n"
+                     if(estimatedClass == testDir):
+                         correctClassEstimation += 1
+
+                 accuracy = (correctClassEstimation / numberOfFiles) * 100
+                 print "Accuracy for ", classDir, ": ", accuracy, "%"
+
 
          return totalCmap
