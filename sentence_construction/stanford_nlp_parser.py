7b2b587c7938ae7e4cf7e58f0bd23ea5c9051c21
Girish <girishc132004@gmail.com>
2015-01-21 22:02:14 -0500

    added completed stanford parser

--- sentence_construction/stanford_nlp_parser.py
+++ sentence_construction/stanford_nlp_parser.py
@@ -1,11 +1,19 @@
 '''
 Stanford NLP Parser
 '''
-__author__ = 'girish'
+from corenlp import batch_parse
+import corenlp
 
+__author__ = 'girish'
 
 if __name__ == '__main__':
     filePath = 'test_text.txt'
     # trainClass = 'acq'
     # filePath = os.path.join(os.curdir, os.pardir, 'train', trainClass, trainClass + '_mega.txt')
     textFile = open(filePath, 'r')
+
+    corenlp_dir = '/home/girish/Python/libraries/corenlp-python/stanford-corenlp-full-2014-08-27'
+    raw_text_directory = "raw_text/"
+    parsed = batch_parse(raw_text_directory, corenlp_dir, raw_output=True)
+    for item in parsed:
+        print item
